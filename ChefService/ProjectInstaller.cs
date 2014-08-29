6727e41dba78ecd4f7475940cbabe109906ca5af
unknown <ncarpenter@LPW7-CNU430911S.corp.epnet.com>
2014-08-29 16:15:45 -0400

    First version of a ChefRunner Service needed for reboot handling on Windows

---
+++ ChefService/ProjectInstaller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration.Install;
+using Microsoft.Win32;
+
+
+namespace ChefService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        private void serviceInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+            string regkey = @"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\" + this.serviceInstaller1.ServiceName;
+
+            //http://stackoverflow.com/a/18476058
+            //This will not be active until the first reboot.  
+            //This doesnt matter since this is being added for reboot functionality anyways
+
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                Registry.SetValue(regkey, "DelayedAutostart", 1, RegistryValueKind.DWord);
+            }
+        }
+    }
+}
---
+++ ChefService/Service1.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace ChefService
+{
+    public partial class ChefService : ServiceBase
+    {
+        public ChefService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Trace.WriteLine("Start Onstart event.");
+            ChefRunner.ChefWrap.PerformChefCall();
+            Trace.WriteLine("End onStart event.  Calling Stop");
+            this.Stop(); //Stop as soon as we are started.
+        }
+        protected override void OnStop()
+        {
+            Trace.WriteLine("Start OnStop event");
+            ChefRunner.ChefWrap.ResetRegistryEntry();
+            Trace.WriteLine("END OnStop event");
+        }
+    }
+}

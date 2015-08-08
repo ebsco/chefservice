0f5ccf95c86d46f4845d1f20239ce1076ea33712
Nicholas Carpenter <ncarpenter@ebsco.com>
2015-08-08 00:41:47 -0400

    Adding first commit of build integration.

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Ebsco")]
+[assembly: AssemblyProduct("EIS ChefService WorkAround")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+
+// Make it easy to distinguish Debug and Release (i.e. Retail) builds;
+// for example, through the file properties window.
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyDescription("Flavor=Debug")] // a.k.a. "Comments"
+#else
+[assembly: AssemblyConfiguration("Retail")]
+[assembly: AssemblyDescription("Flavor=Retail")] // a.k.a. "Comments"
+#endif
+
+[assembly: CLSCompliant(true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Note that the assembly version does not get incremented for every build
+// to avoid problems with assembly binding (or requiring a policy or
+// <bindingRedirect> in the config file).
+//
+// The AssemblyFileVersionAttribute is incremented with every build in order
+// to distinguish one build from another. AssemblyFileVersion is specified
+// in AssemblyVersionInfo.cs so that it can be easily incremented by the
+// automated build process.
+/*
+ * Appears nuget honors this Setting unless informational version is specified
+ *  This needs to be reved whenever we want to increase our CLR version to have a new binding
+ *  This is only used for CLR binding and we can get Nuget versioning to work with the other pieces
+ *  
+ * 
+ */
+[assembly: AssemblyVersion("1.0.0.0")]
